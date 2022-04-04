a8c0cea503f2f32ef81badb204143dcb9ba7efbd
sallahs <93150472+sallahs@users.noreply.github.com>
2022-04-04 16:01:04 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -1,26 +1,39 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-mongoose.Promise = global.Promise;
-
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
-try
-{
-    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
-        console.log("connected"));
-}catch (error)
-{
-    console.log("could not connect");
-}
-mongoose.set('useCreateIndex', true);
-
-//movies schema
-let MovieSchema = new Schema({
-    title: {type: String, required: true, index: { unique: true}},
-    yearReleased: {type: String, required: true},
-    genre: {type: String, required: true},
-    actors: [{actorName: String, characterName: String}]
+const mongoose = require("mongoose");
+const MovieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      "Action",
+      "Adventure",
+      "Comedy",
+      "Drama",
+      "Fantasy",
+      "Horror",
+      "Thriller",
+      "Western",
+    ],
+  },
+  actors: [
+    {
+      actorName: {
+        type: String,
+        required: true,
+      },
+      characterName: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
-
-//return the model to server
-module.exports = mongoose.model('Movie', MovieSchema);
+module.exports = mongoose.model("movies", MovieSchema);
